221d3a28b384c087b818eb1b5e42264623241c5b
Julia Hazer <juliahazer@yahoo.com>
2017-05-29 14:15:38 -0700

    added season_id to leagues and adjusted scraping

--- project/leagues/models.py
+++ project/leagues/models.py
@@ -5,10 +5,14 @@ class League(db.Model):
   __tablename__ = 'leagues'
 
   id = db.Column(db.Integer, primary_key=True)
+  year = db.Column(db.Integer)
   name = db.Column(db.Text)
+  season_id = db.Column(db.Integer)
 
-  def __init__(self, name): 
+  def __init__(self, year, name, season_id):
+    self.year = year 
     self.name = name
+    self.season_id = season_id
 
   def __repr__(self):
-    return "#{}: name: {}".format(self.id, self.name)
+    return "#{}: year: {} - name: {} - season_id: {}".format(self.id, self.year, self.name, self.season_id)
