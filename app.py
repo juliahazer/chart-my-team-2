2ddf359d929c2dc32c32700d5577849d8218dc2b
Julia Hazer <juliahazer@yahoo.com>
2017-05-30 16:44:40 -0700

    added environment variables

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from project import app 
+import os
+
+if os.environ.get('ENV') == 'production':
+    debug = False
+else:
+    debug = True
 
 if __name__ == '__main__':
-  app.run(debug=True, port=3000)
+  app.run(debug=debug, port=3000)
