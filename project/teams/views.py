1c3918a63b32a558c6d20dca7c1d5ef7781b47f7
Julia Hazer <juliahazer@yahoo.com>
2017-05-29 17:48:25 -0700

    fixed team dropdown

--- project/teams/views.py
+++ project/teams/views.py
@@ -17,11 +17,14 @@ def index(id):
   seasons = Season.query.order_by(Season.year.desc(), Season.name.asc()).all()
   leagues = League.query.filter_by(season_id=curr_league.season_id).order_by(League.year.desc(), League.name.asc()).all()
   teams = Team.query.filter_by(league_id=id).order_by(Team.name.asc()).all()
-  print(teams)
-  areas = db.session.query(Team.area.distinct()).all() #NEED TO FIX THIS TO LIMIT IT!!!
-  return render_template('teams/index.html', seasons=seasons, leagues=leagues, curr_league=curr_league, areas=areas, teams=teams)
+  # areas = db.session.query(Team.area.distinct()).all() #NEED TO FIX THIS TO LIMIT IT!!!
+  return render_template('teams/index.html', seasons=seasons, leagues=leagues, curr_league=curr_league, teams=teams)
 
 @teams_blueprint.route('/<int:team_id>')
 def show(id, team_id):
   curr_team = Team.query.get(team_id)
-  return render_template('teams/show.html', team = curr_team)
+  curr_league = League.query.get(id)
+  seasons = Season.query.order_by(Season.year.desc(), Season.name.asc()).all()
+  leagues = League.query.filter_by(season_id=curr_league.season_id).order_by(League.year.desc(), League.name.asc()).all()
+  teams = Team.query.filter_by(league_id=id).order_by(Team.name.asc()).all()
+  return render_template('teams/show.html', seasons=seasons, leagues=leagues, curr_league=curr_league, teams=teams, curr_team =curr_team)
