bf427c954dda71d7c9367571e7ca8fc4abe24e0b
Julia Hazer <juliahazer@yahoo.com>
2017-06-02 14:24:08 -0700

    adding styling for teams page

--- project/__init__.py
+++ project/__init__.py
@@ -33,7 +33,7 @@ app.register_blueprint(rosters_blueprint, url_prefix='/rosters')
 
 @app.route('/')
 def root(): 
-  return redirect(url_for('teams.index'))
+  return render_template('/index.html')
 
 @app.route('/about')
 def about():
